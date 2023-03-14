3931efbd0bd295711c61f1b0da8ee1c80720ed36
Yago <modenayago@yahoo.com>
2023-03-14 13:26:52 -0300

    Adicionar arquivos de projeto.

---
+++ ToggleButton/Controls/ToggleButton.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace ToggleButton.Controls
+{
+    public class ToggleButton : CheckBox
+    {
+        //Fields
+        private Color onBackColor = Color.MediumSlateBlue;
+        private Color onToggleCollor = Color.WhiteSmoke;
+        private Color offBackColor = Color.Gray;
+        private Color offToggleColor = Color.Gainsboro;
+        private bool solidStyle = true;
+
+        //Propriedades
+        public Color OnBackColor
+        {
+            get
+            {
+                return onBackColor;
+            }
+            set
+            {
+                onBackColor = value;
+                this.Invalidate();
+            }
+        }
+        public Color OnToggleCollor
+        {
+            get
+            {
+                return onToggleCollor;
+            }
+            set
+            {
+                onToggleCollor = value;
+                this.Invalidate();
+            }
+        }
+        public Color OffBackColor
+        {
+            get
+            {
+                return offBackColor;
+            }
+            set
+            {
+                offBackColor = value;
+                this.Invalidate();
+            }
+        }
+        public Color OffToggleColor
+        {
+            get
+            {
+                return offToggleColor;
+            }
+            set
+            {
+                offToggleColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+
+            }
+        }
+
+        [DefaultValue(true)]
+        public bool SolidStyle
+        {
+            get
+            {
+                return solidStyle;
+            }
+            set
+            {
+                solidStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        //Constructor
+        public ToggleButton()
+        {
+            this.MinimumSize = new Size(45, 22);
+        }
+
+        //Methods
+        private GraphicsPath GetFigurePath()
+        {
+            int arcSize = this.Height - 1;
+            Rectangle leftArc = new Rectangle(0, 0, arcSize, arcSize);
+            Rectangle rightArc = new Rectangle(this.Width - arcSize - 2, 0, arcSize, arcSize);
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(leftArc, 90, 180);
+            path.AddArc(rightArc, 270, 180);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            int toggleSize = this.Height - 5;
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            pevent.Graphics.Clear(this.Parent.BackColor);
+
+            if (this.Checked)
+            {
+                //DRAW THE CONTROL SURFACE
+                if (solidStyle)
+                    pevent.Graphics.FillPath(new SolidBrush(onBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(OnBackColor, 2), GetFigurePath());
+                //DRAW THE TOGGLE
+                pevent.Graphics.FillEllipse(new SolidBrush(onToggleCollor),
+                    new Rectangle(this.Width - this.Height + 1, 2, toggleSize, toggleSize));
+            }
+            else
+            {
+                //DRAW THE CONTROL SURFACE
+                if (solidStyle)
+                    pevent.Graphics.FillPath(new SolidBrush(offBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(offBackColor, 2), GetFigurePath());
+
+                //DRAW THE TOGGLE
+                pevent.Graphics.FillEllipse(new SolidBrush(offToggleColor),
+                    new Rectangle(2, 2, toggleSize, toggleSize));
+            }
+        }
+    }
+}
---
+++ ToggleButton/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ToggleButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toggleButton1 = new ToggleButton.Controls.ToggleButton();
+            this.toggleButton2 = new ToggleButton.Controls.ToggleButton();
+            this.SuspendLayout();
+            // 
+            // toggleButton1
+            // 
+            this.toggleButton1.AutoSize = true;
+            this.toggleButton1.Location = new System.Drawing.Point(30, 31);
+            this.toggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.toggleButton1.Name = "toggleButton1";
+            this.toggleButton1.OffBackColor = System.Drawing.Color.Gray;
+            this.toggleButton1.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.toggleButton1.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.toggleButton1.OnToggleCollor = System.Drawing.Color.WhiteSmoke;
+            this.toggleButton1.Size = new System.Drawing.Size(45, 22);
+            this.toggleButton1.TabIndex = 0;
+            this.toggleButton1.UseVisualStyleBackColor = true;
+            // 
+            // toggleButton2
+            // 
+            this.toggleButton2.AutoSize = true;
+            this.toggleButton2.Location = new System.Drawing.Point(30, 77);
+            this.toggleButton2.MinimumSize = new System.Drawing.Size(45, 22);
+            this.toggleButton2.Name = "toggleButton2";
+            this.toggleButton2.OffBackColor = System.Drawing.Color.Gray;
+            this.toggleButton2.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.toggleButton2.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.toggleButton2.OnToggleCollor = System.Drawing.Color.Lime;
+            this.toggleButton2.Size = new System.Drawing.Size(45, 22);
+            this.toggleButton2.SolidStyle = false;
+            this.toggleButton2.TabIndex = 2;
+            this.toggleButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 126);
+            this.Controls.Add(this.toggleButton2);
+            this.Controls.Add(this.toggleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.ToggleButton toggleButton1;
+        private Controls.ToggleButton toggleButton2;
+    }
+}
+
